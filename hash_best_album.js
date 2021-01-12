631f79779cb7459678653bbc10b8c097a4ed2072
leedoopal <1233wsh@gmail.com>
2021-01-12 23:29:27 +0900

    completed hash best_album

--- hash_best_album.js
+++ hash_best_album.js
@@ -2,30 +2,47 @@
 
 function solution(genres, plays) {
   let arr = [];
-  let answer = {};
+  let output = {};
   let obj = {};
+  let answer = [];
 
   for (let i in genres) {
-    arr.push([genres[i], plays[i]])
+    arr.push([i, genres[i], plays[i]])
     obj[genres[i]] = 0;
-    answer[genres[i]] = [];
+    output[genres[i]] = [];
   }
 
   arr.forEach(v => {
-    obj[v[0]] += v[1];
+    obj[v[1]] += v[2];
   })
 
-  // console.log(Object.entries(obj).sort((a, b) => b[1] - a[1]));
-
-  let currentKey = arr[0][0];
-  arr.forEach((v, index) => {
-    if (v[0] === currentKey) {
-      answer[v[0]].push({key: index, value: v[1]})
-    } else if (v[0] !== currentKey) {
-      currentKey = v[0];
-      answer[v[0]].push({key: index, value: v[1]})
+  arr.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[2] < b[2] ? 1 : (a[2] > b[2]) ? -1 : 0;
+    } else {
+      return a[2] < b[2] ? 1 : -1;
     }
   })
 
-  return answer;
+  obj = Object.entries(obj).sort((a, b) => b[1] - a[1]).map(v => v[0])
+
+  let currentKey = arr[0][1];
+  let index = 0;
+  arr.forEach((v, index) => {
+    if (v[1] === currentKey && index < 2) {
+      output[v[1]].push(v[0]);
+      index++;
+    } else if (v[1] !== currentKey) {
+      currentKey = v[1];
+      output[v[1]].push(v[0])
+    }
+  })
+
+  obj.forEach((v) => {
+    if (Object.keys(output).find((key) => v === key)) {
+      answer.push(output[v].map(v => parseInt(v)))
+    }
+  })
+
+  return answer.flat();
 }
