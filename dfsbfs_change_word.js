8eec3d8d1ccaf427487c8cf861234eb9da7ac7c7
leedoopal <1233wsh@gmail.com>
2021-01-29 22:33:38 +0900

    modify bfs change word

--- dfsbfs_change_word.js
+++ dfsbfs_change_word.js
@@ -1,20 +1,19 @@
 function solution(begin, target, words) {
-  const targetArr = target.split('');
+  if (!words.includes(target)) return 0;
+  const queue = [begin];
+  const checkArr = new Set();
   let answer = 0;
-  let index = 0;
 
-  function dfs(word) {
-    console.log('word: ', word);
-    const wordArr = word.split('');
-    const filterArr = targetArr.filter((v, i) => v === wordArr[i])
-    if (filterArr[0]) {
-      const filterIndex = targetArr.findIndex(v => v === filterArr[0]);
-      delete targetArr[filterIndex]
-      console.log(targetArr);
-      dfs(words[index + 1])
+  while (queue.length) {
+    const word = queue.shift();
+    checkArr.add(word);
+
+    if (word === target) return answer;
+
+    for (let i = 0; i < word.length; i++) {
+      console.log(word[i]);
     }
   }
-  dfs(words[0])
 
   return answer;
 }
