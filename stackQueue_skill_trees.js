5d4f2dbfc0c52e67cc126644ee12bfe95f99fd05
leedoopal <1233wsh@gmail.com>
2021-02-02 23:11:55 +0900

    modify stack queue skill trees

--- stackQueue_skill_trees.js
+++ stackQueue_skill_trees.js
@@ -1,37 +1,13 @@
 function solution(skill, skill_trees) {
-  const result = new Array(skill_trees.length).fill([]);
-
-  skill_trees.forEach((v, i) => {
-    const input = skill.split('')
-    const arr = v.split('');
-    let index = 0;
-    let current = input[index];
-
-    if (arr.findIndex(x => x === current) > -1) {
-      result[i] = findWord(input, arr, current)
-    } else if (index < skill.length) {
-      result[i] = findWord(input, arr, input[index + 1])
-    }
-  })
-
-  function findWord(input, arr, current) {
-    const output = arr.concat(arr.splice(0, arr.findIndex(x => x === current)));
-    return input.map(y => {
-      return output.findIndex(el => el === y)
-    })
-  }
-
-  const answer = result.map(v => {
-    let temp = v[0];
-    for (let i = 1; i < v.length; i++) {
-      if (temp > v[i]) {
-        return false;
-      } else {
-        temp = v[i];
-      }
+  function checkAnswer(v) {
+    const input = skill.split('');
+    for (let i = 0; i < input.length; i++) {
+      if (!skill.includes(v[i])) continue;
+      if (v[i] === input.shift()) continue;
+      return false;
     }
     return true;
-  });
+  }
 
-  return answer.filter(v => v).length;
+  return skill_trees.filter(checkAnswer).length;
 }
